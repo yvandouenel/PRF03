9b8141b4a24516abbdd5fce4aa196747cb8f56dc
yvandouenel <y.douenel@coopernet.fr>
2020-04-07 12:28:45 +0200

    Component column créé.

--- react/memo/src/components/Column.jsx
+++ react/memo/src/components/Column.jsx
@@ -4,8 +4,12 @@ class Column extends Component {
     state = {}
     render() {
         return (
-            <section className="col-3">
-                <h2>{this.props.column.name}</h2>
+            <section className="col-3 mt-3">
+                <header className="d-flex align-items-center">
+                    <button className="btn btn-success mr-2" >+</button>
+                    <h2 className="h5">{this.props.column.name}</h2>
+                </header>
+
             </section>
         );
     }
