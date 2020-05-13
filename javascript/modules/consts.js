e2cdbbd112ce47988be664fe458093bcc2fe026e
yvan <y.douenel@coopernet.fr>
2020-05-13 17:05:16 +0200

    import et import modifié.

---
+++ javascript/modules/consts.js
@@ -0,0 +1,2 @@
+export const pi = 3.14159265359;
+export const nb_or = 1.61803398875;
--- javascript/modules/main.js
+++ javascript/modules/main.js
@@ -1,8 +1,12 @@
 //import de la class Card
 import Card from "./Card.js";
+//import { pi, nb_or } from "./consts.js";
+import * as consts from "./consts.js";
 
-window.onload = function() {
-    console.log("Dans main.js");
-    const new_card = new Card("Question", "Réponse");
-    console.log(new_card);
+window.onload = function () {
+  console.log("Dans main.js");
+  const new_card = new Card("Question", "Réponse");
+  console.log(new_card);
+  console.log("pi : ", consts.pi);
+  console.log("nb_or : ", consts.nb_or);
 }
